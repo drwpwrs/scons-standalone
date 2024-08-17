9f0f48a7411df05d7315b290dcdbddfcabaafccb
Drew Powers <drew@drwpwrs.com>
2024-08-17 16:52:43 -0400

    fix scons wrapper

--- src/scons_wrapper.py
+++ src/scons_wrapper.py
@@ -2,7 +2,14 @@ import sys
 import os
 
 if __name__ == "__main__":
-    scons_dir = os.path.join(os.path.dirname(sys.executable), 'scons')
-    sys.path.insert(0, scons_dir)
-    import SCons.Script
+    # Check if we're in a PyInstaller bundle
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        # If we are, SCons should already be in sys.path
+        import SCons.Script
+    else:
+        # If we're not, assume we're in a development environment
+        scons_dir = os.path.join(os.path.dirname(sys.executable), 'scons')
+        sys.path.insert(0, scons_dir)
+        import SCons.Script
+    
     SCons.Script.main()
